432382427a65e79690a7f8d77e7afee9b12579ff
Jps-19 <86155000+Jps-19@users.noreply.github.com>
2023-05-29 11:09:35 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginProject.ViewModels;
+
+namespace LoginProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(HomeViewModel hvm)
+        {
+            if (hvm.home.Username.Equals("jayaprakash") && hvm.home.Password.Equals("8658"))
+            {
+                Session["username"] = hvm.home.Username;
+                return View("Welcome");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid Username & Password";
+                return View("Index");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LoginProject.Models;
+
+namespace LoginProject.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Home home
+        {
+            get;
+            set;
+        }
+    }
+}
